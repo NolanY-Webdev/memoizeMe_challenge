205474463d94ad19fde4f87b4f034ab6a20117cd
SkinnyFatMan <SkinnyFatMan@gmail.com>
2015-09-21 13:34:07 -1000

    fixed program

--- memorize_me.js
+++ memorize_me.js
@@ -1,27 +1,25 @@
-function memorize(elementID) {
+var memorize = (function() {
   var things = {};
 
-  (function memoryRetrieve(elementID) {
+  function memoryRetrieve(elementID) {
     if (things.hasOwnProperty(elementID)) {
       return things[elementID];
-    } else {
+    } else if (document.getElementbyID(elementID)) {
       console.log(elementID + ' element not found. Adding now.');
       var element = document.getElementbyID(elementID);
       things[elementID] = element;
       return things[elementID];
+    } else {
+      console.log('ERROR: Not a valid elementID');
     }
-  })();
+  }
 
   function logIt() {
     console.log(things);
   }
 
-
-
-
-
-
-  return({
-    logIt : logIt
-  });
-}
+  return {
+    logIt : logIt,
+    memoryRetrieve : memoryRetrieve
+  };
+})();
